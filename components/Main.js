ba4941575db26167999f10d6d308803ef652e677
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-13 20:57:21 +0900

    💄スタイルファイルの分割

--- components/Main.js
+++ components/Main.js
@@ -1,15 +1,15 @@
 // import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import classes from './Main.module.css'
 import { Links } from '../components/Links'
 import { Headline } from '../components/Headline'
 
 export function Main(props) {
   // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない。
   return (
-      <main className={styles.main}>
+      <main className={classes.main}>
         <Headline page={props.page}>
             {/* onClick={() => alert("クリック！！")} */}
-            <code className={styles.code}>pages/{props.page}.js</code>  
+            <code className={classes.code}>pages/{props.page}.js</code>  
         </Headline>
         <Links />
     </main>
