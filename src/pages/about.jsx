b438e83569cf3d93ffc32dbc4c0015a2a8117865
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-14 22:57:24 +0900

    🐛 importのパス修正

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from 'src/components/styles/Home.module.css'
-import { Footer } from 'src/components/components/Footer'
-import { Main } from 'src/components/components/Main'
-import { Header } from 'src/components/components/Header'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "src/styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
 
 export default function About(props) {
   // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない。
@@ -16,5 +16,5 @@ export default function About(props) {
       <Main page="about" />
       <Footer />
     </div>
-  )
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,32 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import Image from 'next/image'
-import classes from '../styles/Home.module.css'
-import { Footer } from '../components/Footer'
-import { Main } from '../components/Main'
-import { Header } from '../components/Header'
+import classes from "../styles/Home.module.css";
+import { Footer } from "../components/Footer";
+import { Main } from "../components/Main";
+import { Header } from "../components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
-  // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない。
+  // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない
+  const handleClick = useCallback((e) => {
+    // useCallback→再レンダリングされたときに再生成されることがなくなる。第2引数が必要(空の配列を渡す)
+    console.log(e.target.href);
+    // e.target.○○→クリックした要素の属性にアクセスすることができる
+    e.preventDefault();
+  }, []);
+
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
-        <Header />
-        <Main page="index" />
+      <Header />
+      <a href="/about" onClick={handleClick}>
+        {/* return内部でメソッドは書かない。処理が多くなった場合に見た目がわかりづらい */}
+        ボタン
+      </a>
+      <Main page="index" />
       <Footer />
     </div>
-  )
+  );
 }
