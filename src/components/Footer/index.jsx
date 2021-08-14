3f3d4bb817e22c99cbe64f0cac33b9307c8eb5fe
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-14 22:58:48 +0900

    🐛 エラー箇所をコメントアウト

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,19 +1,18 @@
-import classes from 'src/components/Footer/Footer.module.css'
+import classes from "src/components/Footer/Footer.module.css";
 
 export function Footer() {
   return (
-      <footer className={classes.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={classes.logo}>
-            {/* <Img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
-          </span>
-        </a>
-      </footer>
-  )
+    <footer className={classes.footer}>
+      <a
+        // href="https://vercel.com?/ce=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{" "}
+        <span className={classes.logo}>
+          {/* <Img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
+        </span>
+      </a>
+    </footer>
+  );
 }
-
