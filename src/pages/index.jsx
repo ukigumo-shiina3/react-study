ce63b6c656e419adba2d758081552da80cb04cb9
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-15 12:32:11 +0900

    ✨ useEffect導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,10 +4,20 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
-  // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない
+  // // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない
+  console.log("マウント時");
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   const handleClick = useCallback((e) => {
     // useCallback→再レンダリングされたときに再生成されることがなくなる。第2引数が必要(空の配列を渡す)
     console.log(e.target.href);
