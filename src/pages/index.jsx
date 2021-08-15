c1ac31957e115b0f49f3d07ed625e82e87893fd0
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-15 22:17:06 +0900

    ⚡ useCallbackの第2引数

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,23 +4,32 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
   // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(
+    (e) => {
+      // usecallback→再生成されないようにする
+      console.log(count);
+      if (count < 10) {
+        setCount((count) => count + 1);
+        // (count)→前回の数値が入っている
+      }
+    },
+    [count]
+  );
+  // 第2引数に何も指定していなかったら、再生成されないのでcountは1のまま。
+  // 指定した場合関数内の処理が更新され再生成される
 
   useEffect(() => {
     // マウントの処理
-    console.log(`マウント時: ${count}`);
+    // console.log(`マウント時: ${count}`);
     document.body.style.backgroundColor = "lightblue";
     // アンマウントの処理(Cleanup Function)
     return () => {
-      console.log(`アンマウント時: ${count}`);
+      // console.log(`アンマウント時: ${count}`);
       document.body.style.backgroundColor = "";
     };
   }, [count]);
