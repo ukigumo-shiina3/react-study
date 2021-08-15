a882602758a15c1c0f8fe83d1f10f806b5c685c2
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-15 21:35:04 +0900

    ⚡ useEffectの第2引数

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,19 +7,24 @@ import { Header } from "src/components/Header";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [foo, setFoo] = useState(1);
+  const [count, setCount] = useState(1);
   // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない
   const handleClick = (e) => {
-    setFoo((foo) => foo + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
   };
 
   useEffect(() => {
+    // マウントの処理
+    console.log(`マウント時: ${count}`);
     document.body.style.backgroundColor = "lightblue";
-
+    // アンマウントの処理(Cleanup Function)
     return () => {
+      console.log(`アンマウント時: ${count}`);
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [count]);
+  // 第2引数に値を入れると変数が変更されたタイミングで、改めてuseEffectの部分の処理が走る。マウントとアンマウント両方呼ばれる
 
   return (
     <div className={classes.container}>
@@ -27,7 +32,7 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{foo}</h1>
+      <h1>{count}</h1>
       <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
