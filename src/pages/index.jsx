bcc4fd647f55ea90333763b99410108d2ecd511e
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-15 10:26:36 +0900

    🐛 importのパス修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 // import Image from 'next/image'
-import classes from "../styles/Home.module.css";
-import { Footer } from "../components/Footer";
-import { Main } from "../components/Main";
-import { Header } from "../components/Header";
+import classes from "src/styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
 import { useCallback } from "react";
 
 export default function Home() {
