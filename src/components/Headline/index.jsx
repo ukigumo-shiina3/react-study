3b65cdffe8a3e1e0c46e4720adf3a0e32321bfd6
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-22 13:15:55 +0900

    ⚡️ コンポーネント間のStateのリフトアップ

---
+++ src/components/Headline/index.jsx
@@ -0,0 +1,16 @@
+// import Image from 'next/image'
+import classes from "src/components/Headline/Headline.module.css";
+
+export function Headline(props) {
+  return (
+    <div>
+      <h1 className={classes.title}>{props.page} page</h1>
+
+      <p className={classes.description}>
+        アイテムの数は {props.children} 個です。
+      </p>
+      {/* <button onClick={props.onClick}>ボタン</button> */}
+      <button onClick={props.handleReduce}>減らす</button>
+    </div>
+  );
+}
--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,40 +1,16 @@
-import classes from 'src/components/Links/Links.module.css'
+import classes from "src/components/Links/Links.module.css";
 
-const ITEMS = [
-// 不変を表すために大文字の「ITEMS」にする
-  {
-    href: "https://nextjs.org/docs",
-    title: "Documentation →", // HTML Entliesだと「＼u2192」で「→」の代わりになる
-    description: "Find in-depth information about Next.js features and API."
-  },
-  {
-    href: "https://nextjs.org/learn",
-    title: "Learn →",
-    description: "Learn about Next.js in an interactive course with quizzes!"
-  },
-  {
-    href: "https://github.com/vercel/next.js/tree/master/examples",
-    title: "Examples →",
-    description: "Discover and deploy boilerplate example Next.js projects."
-  },
-  {
-    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "Deploy →",
-    description: "Instantly deploy your Next.js site to a public URL with Vercel."
-  }
-]
-
-export function Links() {
+export function Links({ items }) {
   return (
     <div className={classes.grid}>
-        {ITEMS.map((item) => {
-          return (
-            <a key={item.href} href={item.href} className={classes.card}>
-              <h2 className={classes.title}>{item.title}</h2>
-              <p className={classes.description}>{item.description}</p>
-            </a>
-          );
-        })}
+      {items.map((item) => {
+        return (
+          <a key={item.href} href={item.href} className={classes.card}>
+            <h2 className={classes.title}>{item.title}</h2>
+            <p className={classes.description}>{item.description}</p>
+          </a>
+        );
+      })}
     </div>
-  )
+  );
 }
