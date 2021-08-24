076f0742241cfc8f28838a5b592b8272e28cf391
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-24 17:34:21 +0900

    ✨APIの取得とダミーデータの表示

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,35 +4,73 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback, useEffect, useState } from "react";
 
 const Home = (props) => {
   // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない
+
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  // getPostsの中身が変わったらもう一度useEffectの処理が走る
+
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      {props.isShow ? <h1>{props.count}</h1> : null}
-      {/* 三項演算子 null→何も表示させない */}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      {/* アロー関数内ではreturnは省略して1行でかける */}
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      {/* onChange→テキストの変更があったときにその時のイベントを取得 */}
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <div key={item}>{item}</div>;
-        })}
-      </ul>
 
-      <Main page="index" />
-      <Footer />
+      {posts.length > 0 ? (
+        <ol>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ol>
+      ) : null}
     </div>
   );
 };
 
 export default Home;
+
+{
+  /* {props.isShow ? <h1>{props.count}</h1> : null} */
+}
+{
+  /* 三項演算子 null→何も表示させない */
+}
+{
+  /* <button onClick={props.handleClick}>ボタン</button>
+<button onClick={props.handleDisplay}>
+  {props.isShow ? "非表示" : "表示"}
+</button> */
+}
+{
+  /* アロー関数内ではreturnは省略して1行でかける */
+}
+{
+  /* <input type="text" value={props.text} onChange={props.handleChange} /> */
+}
+{
+  /* onChange→テキストの変更があったときにその時のイベントを取得 */
+}
+{
+  /* <button onClick={props.handleAdd}>追加</button>
+<ul>
+  {props.array.map((item) => {
+    return <div key={item}>{item}</div>;
+  })}
+</ul>
+
+<Main page="index" />
+<Footer /> */
+}
