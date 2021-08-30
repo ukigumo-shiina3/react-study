0f1e5cc1b7dbc57bb978e1a951e03a93c20847d8
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-30 21:55:56 +0900

    🎨  SWRによる非同期処理の置き換え

--- src/components/posts/index.jsx
+++ src/components/posts/index.jsx
@@ -1,68 +1,23 @@
-import { useCallback, useEffect, useReducer } from "react";
-
-const initialState = {
-  data: [],
-  loading: true,
-  error: null,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "end":
-      return {
-        ...state,
-        data: action.data,
-        loading: false,
-      };
-    case "error":
-      return {
-        ...state,
-        loading: false,
-        error: action.error,
-      };
-    default:
-      throw new Error("no such action type!");
-  }
-};
+import { usePosts } from "src/hooks/usePosts";
 
 export const Posts = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const { data, error, isLoading, isEmpty } = usePosts();
 
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error("エラーが発生したため、データの取得に失敗しました。");
-      }
-      const json = await res.json();
-      dispatch({ type: "end", data: json });
-    } catch (error) {
-      dispatch({ type: "error", error });
-      // setLoading(false); // 成功、不成功に関わらずローディング状態をfalseにする
-    }
-  }, []);
-  // try catch: 非同期の場合のエラーのハンドリング
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-  // getPostsの中身が変わったらもう一度useEffectの処理が走る
-
-  if (state.loading) {
+  if (isLoading) {
     return <div>ローディング中</div>;
   }
 
-  if (state.error) {
-    return <div>{state.error.message}</div>;
+  if (error) {
+    return <div>{error.message}</div>;
   }
 
-  if (state.data.length === 0) {
+  if (isEmpty) {
     return <div>データは空です</div>;
   }
 
   return (
     <ol>
-      {state.data.map((post) => {
+      {data.map((post) => {
         return <li key={post.id}>{post.title}</li>;
       })}
     </ol>
