a3803a910509caea577f690baaf86e1451d991b7
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-30 13:45:01 +0900

    ⚡️ useReducerによるstate管理

--- src/components/posts/index.jsx
+++ src/components/posts/index.jsx
@@ -1,11 +1,32 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer } from "react";
+
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end":
+      return {
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+    case "error":
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    default:
+      throw new Error("no such action type!");
+  }
+};
 
 export const Posts = () => {
-  // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない
-
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const getPosts = useCallback(async () => {
     try {
@@ -14,11 +35,11 @@ export const Posts = () => {
         throw new Error("エラーが発生したため、データの取得に失敗しました。");
       }
       const json = await res.json();
-      setPosts(json);
+      dispatch({ type: "end", data: json });
     } catch (error) {
-      setError(error);
+      dispatch({ type: "error", error });
+      // setLoading(false); // 成功、不成功に関わらずローディング状態をfalseにする
     }
-    setLoading(false); // 成功、不成功に関わらずローディング状態をfalseにする
   }, []);
   // try catch: 非同期の場合のエラーのハンドリング
 
@@ -27,21 +48,21 @@ export const Posts = () => {
   }, [getPosts]);
   // getPostsの中身が変わったらもう一度useEffectの処理が走る
 
-  if (loading) {
+  if (state.loading) {
     return <div>ローディング中</div>;
   }
 
-  if (error) {
-    return <div>{error.message}</div>;
+  if (state.error) {
+    return <div>{state.error.message}</div>;
   }
 
-  if (posts.length === 0) {
+  if (state.data.length === 0) {
     return <div>データは空です</div>;
   }
 
   return (
     <ol>
-      {posts.map((post) => {
+      {state.data.map((post) => {
         return <li key={post.id}>{post.title}</li>;
       })}
     </ol>
