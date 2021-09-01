58bb4bada71ef7e5ec6d2425b13bc1298a4e1945
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-09-01 12:44:07 +0900

    🚚 ファイルシステムルーティングを試した

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { usePosts } from "src/hooks/usePosts";
 
 export const Posts = () => {
@@ -18,7 +19,13 @@ export const Posts = () => {
   return (
     <ol>
       {data.map((post) => {
-        return <li key={post.id}>{post.title}</li>;
+        return (
+          <li key={post.id}>
+            <Link href={`/post/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        );
       })}
     </ol>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,8 @@ import Head from "next/head";
 import classes from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
 import { Posts } from "src/components/Posts";
-import { useCallback, useEffect, useState } from "react";
 
 const Home = () => {
-  // ファイルシステムルーティングを機能させるためにpages配下のファイルはexport defaultにしなければいけない
-
   return (
     <div className={classes.container}>
       <Head>
@@ -19,36 +16,3 @@ const Home = () => {
 };
 
 export default Home;
-
-{
-  /* {props.isShow ? <h1>{props.count}</h1> : null} */
-}
-{
-  /* 三項演算子 null→何も表示させない */
-}
-{
-  /* <button onClick={props.handleClick}>ボタン</button>
-<button onClick={props.handleDisplay}>
-  {props.isShow ? "非表示" : "表示"}
-</button> */
-}
-{
-  /* アロー関数内ではreturnは省略して1行でかける */
-}
-{
-  /* <input type="text" value={props.text} onChange={props.handleChange} /> */
-}
-{
-  /* onChange→テキストの変更があったときにその時のイベントを取得 */
-}
-{
-  /* <button onClick={props.handleAdd}>追加</button>
-<ul>
-  {props.array.map((item) => {
-    return <div key={item}>{item}</div>;
-  })}
-</ul>
-
-<Main page="index" />
-<Footer /> */
-}
