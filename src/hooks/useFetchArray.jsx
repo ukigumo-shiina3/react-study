031679893f6f0c4314decae9c9c0ab47035a43ba
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-10-11 11:32:20 +0900

    ✨  SWRのImmutableを使用しリクエスト数を抑える

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -1,7 +1,7 @@
-import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 
 const useFetchArray = (url) => {
-  const { data, error } = useSWR(url);
+  const { data, error } = useSWRImmutable(url);
   return {
     data,
     error,
