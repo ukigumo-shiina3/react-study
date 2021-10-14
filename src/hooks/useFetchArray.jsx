03330c71570a1e6db12a74c7f2424aa358618c20
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-10-14 10:25:05 +0900

    🎨  APIのURLをconstから読み込むように変更

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -1,3 +1,4 @@
+import { API_URL } from "src/utils/const";
 import useSWRImmutable from "swr/immutable";
 
 const useFetchArray = (url) => {
@@ -9,8 +10,6 @@ const useFetchArray = (url) => {
     isEmpty: data && data.length === 0,
   };
 };
-
-const API_URL = "https://jsonplaceholder.typicode.com";
 
 // posts
 export const usePosts = () => {
--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,11 +1,10 @@
 import { Header } from "src/components/Header";
 import { Comment as CommentsComponent } from "src/components/Comment";
 import { SWRConfig } from "swr";
+import { API_URL } from "src/utils/const";
 
 export const getStaticPaths = async () => {
-  const comments = await fetch(
-    "https://jsonplaceholder.typicode.com/comments?_limit=10"
-  );
+  const comments = await fetch(`${API_URL}/comments?_limit=10`);
   const commentsData = await comments.json();
   const paths = commentsData.map((comment) => ({
     params: { id: comment.id.toString() },
@@ -16,7 +15,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (ctx) => {
   const { id } = ctx.params;
-  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const COMMENT_API_URL = `${API_URL}/comments/${id}`;
   const comment = await fetch(COMMENT_API_URL);
 
   if (!comment.ok) {
